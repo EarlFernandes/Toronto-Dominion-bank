fe60bcc4b113d613cc95811507bdb2dc6e3a116b
mulla25 <ashraf.mulla@td.com>
2017-07-19 15:52:31 +0530

    update in MainScreen

--- TDAPP/src/com/td/MainScreen.java
+++ TDAPP/src/com/td/MainScreen.java
@@ -32,7 +32,7 @@ public class MainScreen extends _CommonPage {
 			"SecurityPassword", "TriggerDelta", "CDNMarginAccount", "QuantityType", "Dividend", "SelectLimitPrice",
 			"ConnectID", "Sender", "Ordervalue", "LimitDelta", "TriggerPrice", "Language", "Commission", "CardName",
 			"Passcode", "NewPasscode", "Email", "Name", "EmailProfile", "PhoneProfile", "PostSurveyText", "Response",
-			"UserProfileType" };
+			"UserProfileType","SecurityQuestion","Amount" };
 
 	public void readSheet() {
 		CL.getTestDataInstance().TCParameters = new HashMap<String, String>();
