638f8ef405c13d1006ceb30afb096d0028a8f9fe
hochil5 <hochil5@TDL05106453.TDBFG.com>
2017-11-21 14:06:57 -0500

    webview change

--- TDAPP/src/com/td/MobileAction2.java
+++ TDAPP/src/com/td/MobileAction2.java
@@ -3581,7 +3581,7 @@ public class MobileAction2 extends CommonLib {
 
 		if (lastestContextView.contains("WEBVIEW")) {
 			System.out.println("Before Webview Switch: " + lastestContextView);
-			((AppiumDriver) GetDriver()).context("WEBVIEW_R");
+			((AppiumDriver) GetDriver()).context(lastestContextView);
 			sleep(5000); // WebView needs time to load
 			System.out.println("After Webview Switch");
 
