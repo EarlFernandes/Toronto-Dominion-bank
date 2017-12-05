2522c3ffd3ce6223fed100086fe1e913a4905be6
hochil5 <hochil5@TDL05106453.TDBFG.com>
2017-12-04 20:39:04 -0500

    addPayee change

--- TDAPP/src/com/td/test/CDNMobile/pages/AddPayee.java
+++ TDAPP/src/com/td/test/CDNMobile/pages/AddPayee.java
@@ -61,8 +61,8 @@ public class AddPayee extends _CommonPage {
 	private WebElement payeeContinueBtn;
 
 	@iOSFindBy(xpath = "//XCUIElementTypeWebView[1]//XCUIElementTypeButton[1]")
-	// @FindBy(xpath = "//button[contains(@ng-click,'addPayee')]")
-	@FindBy(xpath = "//div[@class='button-row']")
+	@FindBy(xpath = "//button[@ng-click='addPayee()']")
+	// @FindBy(xpath = "//div[@class='button-row']")
 	private WebElement addPayeeBtn;
 
 	@iOSXCUITFindBy(xpath = "//XCUIElementTypeWebView[1]//XCUIElementTypeButton[1]")
@@ -929,7 +929,8 @@ public class AddPayee extends _CommonPage {
 
 				mobileAction.switchToWebView();
 				mobileAction.FuncClick(addPayeeBtn, "Add Payee button");
-				mobileAction.sleep(10000);
+				Thread.sleep(10000);
+				// mobileAction.sleep(10000);
 
 			} else {
 				mobileAction.FuncClickDone();
